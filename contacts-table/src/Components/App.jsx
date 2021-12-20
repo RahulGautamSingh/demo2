4a41f235922c94c752ed711236ce394395943923
RahulGautamSingh <rahultesnik@gmail.com>
2021-12-20 16:04:37 +0545

    first

--- contacts-table/src/Components/App.jsx
+++ contacts-table/src/Components/App.jsx
@@ -2,17 +2,15 @@ import InputArea from "./InputArea";
 import DisplayArea from "./DisplayArea";
 import { ContactContextProvider } from "./Context";
 
-export default function App(props)
-{
-    return (
-        <ContactContextProvider>
+export default function App(props) {
+  return (
+    <ContactContextProvider>
       <div className="container">
-          <h1>React Hooks Context Demo</h1>
-        <h3>Contacts</h3>
-        <InputArea/>
-        <DisplayArea />
+        <h1>Demo</h1>
 
+        <InputArea />
+        <DisplayArea />
       </div>
-      </ContactContextProvider>
-    )
+    </ContactContextProvider>
+  );
 }
